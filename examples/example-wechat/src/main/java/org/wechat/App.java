e9221b0b9e01d13487738eb6337771e081762099
tommy <276386551@qq.com>
2023-03-04 09:38:07 +0800

    改成集成式neo4j

--- examples/example-wechat/src/main/java/org/wechat/App.java
+++ examples/example-wechat/src/main/java/org/wechat/App.java
@@ -8,13 +8,13 @@ import java.io.IOException;
 public class App {
     public static void main(String[] args) throws IOException {
        /*
-       集成数据库
+       集成数据库*/
         String temDir = System.getProperty("java.io.tmpdir");
         File file = new File(temDir, "neo4jDb_Wechat");
         String url=file.toURI().toString();
         System.out.println("数据库："+file);
-        */
-       String url="http://127.0.0.1:7474/";
+
+//       String url="http://127.0.0.1:7474/";
         new HttpServer()
                 .api("org.wechat.service")
                 .dbConfig(url, "neo4j", "123456", 10,
